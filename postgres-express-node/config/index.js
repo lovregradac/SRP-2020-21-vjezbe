ca38e80a82dabe5926d88bb7c5900248ef1917d3
lgrada00 <lgrada00@fesb.hr>
2020-12-02 10:29:18 +0100

    Add JWT token verification

--- postgres-express-node/config/index.js
+++ postgres-express-node/config/index.js
@@ -29,6 +29,15 @@ module.exports = {
 
   jwt: {
       secret: process.env.JWT_SECRET,
-      expiresIn: process.env.JWT_DURATION
-  }
+      expiresIn: process.env.JWT_DURATION || "1h",
+      algorithms: ["HS256"],
+      exclude: {
+          path: [
+              {
+                  url: "/api/login",
+                  methods: ["POST"],
+              },
+          ],
+      },
+  },  
 };
